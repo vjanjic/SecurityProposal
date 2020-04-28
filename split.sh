396821d839b08d2253932e472ba9550429dcddb7
Vladimir Janjic <vjanjic@gmail.com>
2020-04-28 17:40:29 +0100

    First version of WP3

--- split.sh
+++ split.sh
@@ -1,3 +1,3 @@
-pdftk SecurityProposal.pdf cat 1-36 output SecurityProposal-1-3.pdf dont_ask
-pdftk SecurityProposal.pdf cat 37-43 output SecurityProposal-4-5.pdf dont_ask
+pdftk SecurityProposal.pdf cat 1-37 output SecurityProposal-1-3.pdf dont_ask
+pdftk SecurityProposal.pdf cat 38-44 output SecurityProposal-4-5.pdf dont_ask
 exit
