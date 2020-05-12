af1fa65dcea6210463433ff11deffb418e7c7d4b
vjanjic <vj32@st-andrews.ac.uk>
2020-05-12 17:47:40 +0100

    Updates from Overleaf

--- split.sh
+++ split.sh
@@ -1,3 +1,3 @@
-pdftk SecurityProposal.pdf cat 1-37 output SecurityProposal-1-3.pdf dont_ask
-pdftk SecurityProposal.pdf cat 38-44 output SecurityProposal-4-5.pdf dont_ask
+pdftk SecurityProposal.pdf cat 1-44 output SecurityProposal-1-3.pdf dont_ask
+pdftk SecurityProposal.pdf cat 45-53 output SecurityProposal-4-5.pdf dont_ask
 exit
