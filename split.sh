d57603f5fbe385488f4bbf54e310f6ae8220338d
Vladimir Janjic <vjanjic@gmail.com>
2020-06-23 19:40:13 +0100

    minor change

--- split.sh
+++ split.sh
@@ -1,3 +1,3 @@
-pdftk SecurityProposal.pdf cat 1-60 output SecurityProposal-1-3.pdf dont_ask
-pdftk SecurityProposal.pdf cat 61-69 output SecurityProposal-4-5.pdf dont_ask
+pdftk SecurityProposal.pdf cat 1-61 output SecurityProposal-1-3.pdf dont_ask
+pdftk SecurityProposal.pdf cat 62-70 output SecurityProposal-4-5.pdf dont_ask
 exit
